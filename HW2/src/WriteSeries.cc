b89722583e7751e9114c90940b00fc9ceb45fc39
ozge orhan <ozgeorhanolsun@gmail.com>
2022-11-02 18:10:33 +0100

    WriteSeries class with sample output files

---
+++ HW2/src/WriteSeries.cc
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "WriteSeries.hh"
+
+/*!
+ * @brief constructor
+ * @param series series taken into consideration
+ * @param maxIter maximum iteration
+ */
+WriteSeries::WriteSeries(Series& series, unsigned long maxIter) : DumperSeries(series) {
+    this->maxIter = maxIter;
+};
+
+/*!
+ * @brief setting the separator for the output file
+ * @param c separator character
+ */
+void WriteSeries::setSeparator(char c) {
+    this->separator = c;
+};
+
+/*!
+ * @brief dump method to output
+ * @param os output stream
+ */
+void WriteSeries::dump(std::ostream & os){
+
+    std::string fileName = "output";
+    std::string relativePath = "..\\..\\";
+    std::string fileExt = "";
+
+
+    if(this->separator == ','){
+        fileExt += ".csv";
+        separator = ',';
+    }
+    else if(this->separator == ' ' || this->separator == '\t'){
+        fileExt += ".txt";
+        separator = ' ';
+    }
+    else if(this->separator == '|'){
+        fileExt += ".psv";
+        separator = '|';
+    }
+    else{
+        std::cout << "Invalid separator" << std::endl;
+    }
+
+
+    relativePath = relativePath + fileName + fileExt;
+    std::cout << relativePath << std::endl;
+
+    std::ofstream myFile(relativePath);
+
+    myFile << "Steps" << this->separator << "Convergence Value" << this->separator << "Analytical prediction" <<"\n";
+
+    if (myFile.is_open())
+    {
+        for (unsigned long i = 1; i <= this->maxIter; i++) {
+                myFile << i << this->separator << series.compute(i) << this->separator << series.getAnalyticPrediction() << std::endl;
+        }
+
+        myFile.close();
+    }
+    else std::cout << "Unable to open file";
+
+};
---
+++ HW2/src/WriteSeries.hh
@@ -0,0 +1,25 @@
+#include "DumperSeries.hh"
+#include "Series.hh"
+
+#ifndef HOMEWORK2_WRITESERIES_H
+#define HOMEWORK2_WRITESERIES_H
+
+/*!
+ * @brief class definition for writing series to output files
+ */
+class WriteSeries : public DumperSeries {
+public:
+    WriteSeries(Series& series, unsigned long maxIter);
+    virtual ~WriteSeries() {};
+
+    unsigned int maxIter;
+    unsigned int freq;
+    void dump(std::ostream & os = std::cout) override;
+    void setSeparator(char c);
+
+protected:
+    char separator = ' ';
+};
+
+
+#endif //HOMEWORK2_WRITESERIES_H
