3ee5388ea7bcdf6d8578c8bcec54b8fd5cdbc8bf
Gizem Ozdil <pembe.ozdil@epfl.ch>
2022-12-14 19:28:12 +0100

    [HW3] add plotting option

--- HW3/src/heat_distribution.py
+++ HW3/src/heat_distribution.py
@@ -1,12 +1,14 @@
 """
     Script to generate a CSV file for the heat distribution.
     Example usage:
-    >>> python heat_distribution.py --xlim -1 1 --ylim -1 1 --pn_x 8 --pn_y 8 --radius 0.5 --filename heat_file_input.csv
+    >>> python heat_distribution.py --xlim -1 1 --ylim -1 1 --pn_x 8 --pn_y 8 --radius 0.5 --filename heat_file_input.csv --plot
 
     File contains: [positions (N,3) velocities (N,3) force (N,3) masses (N,1) temperature (N,1) heat rate (n,3)]
 """
 import argparse
 import numpy as np
+
+import matplotlib.pyplot as plt
 
 
 def arg_parser():
@@ -15,6 +17,7 @@ def arg_parser():
     parser.add_argument('--ylim', action='store', type=float, default=[-1.,1.], nargs='+', help='Upper and lower limits for the y axis')
     parser.add_argument('--pn_x', action='store', type=int, default=8, help="Number of particlesi in x direction")
     parser.add_argument('--pn_y', action='store', type=int, default=8, help="Number of particlesi in y direction")
+    parser.add_argument('--plot', action='store_true', help="Plots the heat distribution")
     parser.add_argument(
         '--radius', action='store', type=float, default=0.5,
         help="Radius of the heat distribution")
@@ -60,6 +63,29 @@ def boundary_constraint(x, y, xlim, ylim):
     return np.random.randint(-10,10)
 
 
+def plot_heat_distribution(nx, ny, temperature):
+    """Plots the heat distribution with temperature color code.
+
+    Parameters
+    ----------
+    nx : int
+        Number of particlesi in x direction
+    ny : int
+        Number of particlesi in y direction
+    temperature : NDarray
+        Array containing the temperature values
+    """
+
+    fig, ax = plt.subplots()
+    plt.pcolormesh(temperature.reshape((nx,ny)))
+    cbar = plt.colorbar()
+    cbar.set_label('temperature', rotation=90)
+    plt.xlabel('Number of particles  in x')
+    plt.ylabel('Number of particles in y')
+    plt.title('Temperature in a grid')
+    plt.show()
+
+
 def main():
     args = arg_parser()
 
@@ -69,6 +95,7 @@ def main():
     pn_y = args.pn_y
     radius = args.radius
     fname = args.filename
+    plot = args.plot
 
     grid_size = pn_x * pn_y
 
@@ -93,6 +120,9 @@ def main():
     file_data = np.hstack((positions, velocity, force, masses, temperature, heat_rate))
     np.savetxt(fname, file_data, delimiter=" ")
     print("Heat distribution file is saved at {}".format(fname))
+
+    if plot:
+        plot_heat_distribution(pn_x, pn_y, temperature)
 
 
 if __name__ == "__main__":
