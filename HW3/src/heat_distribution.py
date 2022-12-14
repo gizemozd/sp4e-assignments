253cddcbaa3477893159c8b587db0e2b7f736756
Gizem Ozdil <pembe.ozdil@epfl.ch>
2022-12-14 19:03:42 +0100

    [HW3] python heat distribution generator added

--- HW3/src/heat_distribution.py
+++ HW3/src/heat_distribution.py
@@ -1,36 +1,99 @@
+"""
+    Script to generate a CSV file for the heat distribution.
+    Example usage:
+    >>> python heat_distribution.py --xlim -1 1 --ylim -1 1 --pn_x 8 --pn_y 8 --radius 0.5 --filename heat_file_input.csv
+
+    File contains: [positions (N,3) velocities (N,3) force (N,3) masses (N,1) temperature (N,1) heat rate (n,3)]
+"""
 import argparse
 import numpy as np
 
 
 def arg_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--xlim', action='store', type=float, default=[-1.0, 1.0], help="lower and upper bounds of x")
-    parser.add_argument('--ylim', action='store', type=float, nargs='+', default=[-1.0, 1.0], help="lower and upper bounds of y")
-    parser.add_argument('--size',  action='store', type=int, default=512, help="size of the grid")
-    parser.add_argument('--radius',  action='store', type=float, default=0.5,help="radius of area f particles")
-    parser.add_argument('-fname', action='store', default="heatDistribution.csv",help="file name")
+    parser.add_argument('--xlim', action='store', type=float, default=[-1.,1.], nargs='+', help='Upper and lower limits for the x axis')
+    parser.add_argument('--ylim', action='store', type=float, default=[-1.,1.], nargs='+', help='Upper and lower limits for the y axis')
+    parser.add_argument('--pn_x', action='store', type=int, default=8, help="Number of particlesi in x direction")
+    parser.add_argument('--pn_y', action='store', type=int, default=8, help="Number of particlesi in y direction")
+    parser.add_argument(
+        '--radius', action='store', type=float, default=0.5,
+        help="Radius of the heat distribution")
+    parser.add_argument(
+        '--filename', action='store', default="heat_distribution.csv",
+        help="Output path where the heat data will be saved")
     return parser.parse_args()
 
+def heat_func(x, y, R):
+    """ Heat function described in EX 4-5.
 
-# heat distribution function
-def heatDistribution(xlim, ylim, size, radius, fname):
-    x_vals = np.linspace(xlim[0], xlim[1], size)
-    y_vals = np.linspace(ylim[0], ylim[1], size)
-    heat_vals = []
+    Parameters
+    ----------
+    x : float
+        x coordinates
+    y : float
+        y coordinates
+    R : float
+        radius
+    """
+    if  x**2 + y**2 < R:
+        return 1
+
+    return 0
+
+def boundary_constraint(x, y, xlim, ylim):
+    """ Enforces 0 temperature at the boundary and gives a random value otherwise.
+
+    Parameters
+    ----------
+    x : float
+        x coordinates
+    y : float
+        y coordinates
+    xlim : NDArray
+        x boundaries
+    ylim : NDArray
+        y boundaries
+    """
+    if  (x == min(xlim) or x == max(xlim) or y == min(ylim) or y == max(ylim)):
+        return 0
+
+    return np.random.randint(-10,10)
+
+
+def main():
+    args = arg_parser()
+
+    xlim = args.xlim
+    ylim = args.ylim
+    pn_x = args.pn_x
+    pn_y = args.pn_y
+    radius = args.radius
+    fname = args.filename
+
+    grid_size = pn_x * pn_y
+
+    x_vals = np.linspace(xlim[0], xlim[1], pn_x)
+    y_vals = np.linspace(ylim[0], ylim[1], pn_y)
+
+    positions = np.random.random((grid_size, 3))
+    velocity = np.zeros((grid_size, 3))
+    force = velocity.copy()
+    masses = 1e9*(np.random.random((grid_size, 1)) + 1)
+    heat_rate = np.zeros((grid_size, 1))
+    temperature = np.zeros((grid_size, 1))
+
+    ind = 0
     for x in x_vals:
         for y in y_vals:
-            r = x**2 + y**2
-            if r < radius:
-                heat_vals += [[x, y]]
-            else:
-                heat_vals += [[0, 0]]
-    heat_vals = np.asarray(heat_vals)
-    np.savetxt(fname, heat_vals, delimiter=',')
-    return
+            positions[ind,:] = np.array([x,y,0])
+            temperature[ind] = boundary_constraint(x,y,xlim,ylim)
+            heat_rate[ind] = heat_func(x,y,radius)
+            ind += 1
+
+    file_data = np.hstack((positions, velocity, force, masses, temperature, heat_rate))
+    np.savetxt(fname, file_data, delimiter=" ")
+    print("Heat distribution file is saved at {}".format(fname))
 
 
 if __name__ == "__main__":
-    args = arg_parser()
-    print(args)
-    heatDistribution(xlim=args.xlim, ylim=args.ylim, size=args.size, radius=args.radius, fname=args.fname)
-    print("heat distribution file is generated")
+    main()
